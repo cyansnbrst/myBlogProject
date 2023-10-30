7825a0fe900f7dd1330d96333b0223b737927a24
Kat/ <yuuonx@mail.ru>
2023-10-30 17:47:06 +0300

    Added unique models' url and pagination

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,28 @@
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
-
 from blog.models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 4)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
